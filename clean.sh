8b2f023c4c72b06ebe8b2270ce4dbf9c3eed8722
digiprospector <digiprospector@protonmail.com>
2025-08-09 22:05:55 +0800

    update

---
+++ clean.sh
@@ -0,0 +1,5 @@
+BASE_DIR="/content/driver/Mydriver"
+TXT_DIR="$BASE_DIR/audio2txt"
+
+rm -rf "$TXT_DIR/*"
+touch "$TXT_DIR/input.txt"
---
+++ zip.sh
@@ -0,0 +1,4 @@
+BASE_DIR="/content/driver/Mydriver"
+TXT_DIR="$BASE_DIR/audio2txt"
+
+zip -r "$TXT_DIR.zip" "$TXT_DIR"
